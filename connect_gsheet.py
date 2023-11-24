adb7c426694985e72fad24c63922800a514476bd
Antony <antua.froloff@gmail.com>
2023-11-24 14:32:17 +0300

    132

--- connect_gsheet.py
+++ connect_gsheet.py
@@ -1,91 +1,204 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import PySimpleGUI as sg
+import pyperclip
 
-def get_gsheet_data():
+
+def get_checklist_for_partner():
+    # row_list = ['Name', 'CMS key', 'Основа', f'{partner_name}']
+    # col_list = [['По телефону по умолчанию', 'isDefaultLoginByPhone = true', False, True],
+    #             ['E-mail и ID', 'isAllowedLoginByEmailAndId = true', False, True],
+    #             ['Логин', 'isAllowedLoginByLogin = false', False, True],
+    #             ['Ввод кода страны вручную', 'isAllowedCustomPhoneCodeInput = true', False, True],
+    #             ['Через соц. сети', 'isAllowedLoginBySocial = false', False, True],
+    #             ['По телефону по умолчанию', 'isDefaultLoginByPhone = true', False, True],
+    #             ['E-mail и ID', 'isAllowedLoginByEmailAndId = true', False, True],
+    #             ['Логин', 'isAllowedLoginByLogin = false', False, True],
+    #             ['Ввод кода страны вручную', 'isAllowedCustomPhoneCodeInput = true', False, True],
+    #             ['Через соц. сети', 'isAllowedLoginBySocial = false', False, True],
+    #             ['По телефону по умолчанию', 'isDefaultLoginByPhone = true', False, True],
+    #             ['E-mail и ID', 'isAllowedLoginByEmailAndId = true', False, True],
+    #             ['Логин', 'isAllowedLoginByLogin = false', False, True],
+    #             ['Ввод кода страны вручную', 'isAllowedCustomPhoneCodeInput = true', False, True],
+    #             ['Через соц. сети', 'isAllowedLoginBySocial = false', False, True],
+    #             ['По телефону по умолчанию', 'isDefaultLoginByPhone = true', False, True],
+    #             ['E-mail и ID', 'isAllowedLoginByEmailAndId = true', False, True],
+    #             ['Логин', 'isAllowedLoginByLogin = false', False, True],
+    #             ['Ввод кода страны вручную', 'isAllowedCustomPhoneCodeInput = true', False, True],
+    #             ['Через соц. сети', 'isAllowedLoginBySocial = false', False, True],
+    #             ['По телефону по умолчанию', 'isDefaultLoginByPhone = true', False, True],
+    #             ['E-mail и ID', 'isAllowedLoginByEmailAndId = true', False, True],
+    #             ['Логин', 'isAllowedLoginByLogin = false', False, True],
+    #             ['Ввод кода страны вручную', 'isAllowedCustomPhoneCodeInput = true', False, True],
+    #             ['Через соц. сети', 'isAllowedLoginBySocial = false', False, True],
+    #             ['По телефону по умолчанию', 'isDefaultLoginByPhone = true', False, True],
+    #             ['E-mail и ID', 'isAllowedLoginByEmailAndId = true', False, True],
+    #             ['Логин', 'isAllowedLoginByLogin = false', False, True],
+    #             ['Ввод кода страны вручную', 'isAllowedCustomPhoneCodeInput = true', False, True],
+    #             ['Через соц. сети', 'isAllowedLoginBySocial = false', False, True],
+    #             ['По телефону по умолчанию', 'isDefaultLoginByPhone = true', False, True],
+    #             ['E-mail и ID', 'isAllowedLoginByEmailAndId = true', False, True],
+    #             ['Логин', 'isAllowedLoginByLogin = false', False, True],
+    #             ['Ввод кода страны вручную', 'isAllowedCustomPhoneCodeInput = true', False, True],
+    #             ['Через соц. сети', 'isAllowedLoginBySocial = false', False, True],
+    #             ['По телефону по умолчанию', 'isDefaultLoginByPhone = true', False, True],
+    #             ['E-mail и ID', 'isAllowedLoginByEmailAndId = true', False, True],
+    #             ['Логин', 'isAllowedLoginByLogin = false', False, True],
+    #             ['Ввод кода страны вручную', 'isAllowedCustomPhoneCodeInput = true', False, True],
+    #             ['Через соц. сети', 'isAllowedLoginBySocial = false', False, True],
+    #             ['По телефону по умолчанию', 'isDefaultLoginByPhone = true', False, True],
+    #             ['E-mail и ID', 'isAllowedLoginByEmailAndId = true', False, True],
+    #             ['Логин', 'isAllowedLoginByLogin = false', False, True],
+    #             ['Ввод кода страны вручную', 'isAllowedCustomPhoneCodeInput = true', False, True],
+    #             ['Через соц. сети', 'isAllowedLoginBySocial = false', False, True],
+    #             ['По телефону по умолчанию', 'isDefaultLoginByPhone = true', False, True],
+    #             ['E-mail и ID', 'isAllowedLoginByEmailAndId = true', False, True],
+    #             ['Логин', 'isAllowedLoginByLogin = false', False, True],
+    #             ['Ввод кода страны вручную', 'isAllowedCustomPhoneCodeInput = true', False, True],
+    #             ['Через соц. сети', 'isAllowedLoginBySocial = false', False, True],
+    #             ['По телефону по умолчанию', 'isDefaultLoginByPhone = true', False, True],
+    #             ['E-mail и ID', 'isAllowedLoginByEmailAndId = true', False, True],
+    #             ['Логин', 'isAllowedLoginByLogin = false', False, True],
+    #             ['Ввод кода страны вручную', 'isAllowedCustomPhoneCodeInput = true', False, True],
+    #             ['Через соц. сети', 'isAllowedLoginBySocial = false', False, True],
+    #             ['По телефону по умолчанию', 'isDefaultLoginByPhone = true', False, True],
+    #             ['E-mail и ID', 'isAllowedLoginByEmailAndId = true', False, True],
+    #             ['Логин', 'isAllowedLoginByLogin = false', False, True],
+    #             ['Ввод кода страны вручную', 'isAllowedCustomPhoneCodeInput = true', False, True],
+    #             ['Через соц. сети', 'isAllowedLoginBySocial = false', False, True],
+    #             ['По телефону по умолчанию', 'isDefaultLoginByPhone = true', False, True],
+    #             ['E-mail и ID', 'isAllowedLoginByEmailAndId = true', False, True],
+    #             ['Логин', 'isAllowedLoginByLogin = false', False, True],
+    #             ['Ввод кода страны вручную', 'isAllowedCustomPhoneCodeInput = true', False, True],
+    #             ['Через соц. сети', 'isAllowedLoginBySocial = false', False, True],
+    #             ['По телефону по умолчанию', 'isDefaultLoginByPhone = true', False, True],
+    #             ['E-mail и ID', 'isAllowedLoginByEmailAndId = true', False, True],
+    #             ['Логин', 'isAllowedLoginByLogin = false', False, True],
+    #             ['Ввод кода страны вручную', 'isAllowedCustomPhoneCodeInput = true', False, True],
+    #             ['Через соц. сети', 'isAllowedLoginBySocial = false', False, True],
+    #             ['По телефону по умолчанию', 'isDefaultLoginByPhone = true', False, True],
+    #             ['E-mail и ID', 'isAllowedLoginByEmailAndId = true', False, True],
+    #             ['Логин', 'isAllowedLoginByLogin = false', False, True],
+    #             ['Ввод кода страны вручную', 'isAllowedCustomPhoneCodeInput = true', False, True],
+    #             ['Через соц. сети', 'isAllowedLoginBySocial = false', False, True],
+    #             ['По телефону по умолчанию', 'isDefaultLoginByPhone = true', False, True],
+    #             ['E-mail и ID', 'isAllowedLoginByEmailAndId = true', False, True],
+    #             ['Логин', 'isAllowedLoginByLogin = false', False, True],
+    #             ['Ввод кода страны вручную', 'isAllowedCustomPhoneCodeInput = true', False, True],
+    #             ['Через соц. сети', 'isAllowedLoginBySocial = false', False, True],
+    #             ['По телефону по умолчанию', 'isDefaultLoginByPhone = true', False, True],
+    #             ['E-mail и ID', 'isAllowedLoginByEmailAndId = true', False, True],
+    #             ['Логин', 'isAllowedLoginByLogin = false', False, True],
+    #             ['Ввод кода страны вручную', 'isAllowedCustomPhoneCodeInput = true', False, True],
+    #             ['Через соц. сети', 'isAllowedLoginBySocial = false', False, True]
+    #             ]
+    
+        
+    row_list = ['Name', 'CMS key', 'Основа', f'{partner_name}']
+    col_list = [[], [], [], []]
+    col_list[2] = sheet.col_values(13)
+    
+    print(col_list)
+    return row_list, col_list
+
+def get_partner_names():
+    temp_list = sheet.row_values(1)
+    except_list = ['','Функционал на сайте', 'Базовый', 'Android', 'iOS', 'Ключ в CMS', 'Настраивается']
+    partners = []
+    
+    for i in temp_list:
+        if i not in partners:
+            if i not in except_list:
+                partners.append(i)
+    
+    layout = [
+        [sg.Text(f"Choose partner (available = {len(partners)})")],
+        [sg.DropDown(values=partners, default_value=partners[0], auto_size_text=True, key='-DROPDOWN-')],
+        [sg.Button('get', size=10)]
+    ]
+    
+    window = sg.Window('partners list', layout)
+    while True:
+        event, value = window.read(close=True)
+        
+        if event == 'get':
+            selected_partner = value['-DROPDOWN-']
+            return selected_partner
+            
+        if event in ('Cancel', None) or event == sg.WIN_CLOSED:
+            exit(0)
+        window.close()
+
+def show_tablet():
+    row_list, col_list = get_checklist_for_partner()
+    layout = [
+        [sg.Table(
+            values = col_list, headings=row_list,
+            auto_size_columns = 1,
+            vertical_scroll_only = 0,
+            justification = 'center',
+            num_rows = 30,
+            key = 'tablet',
+            selected_row_colors=('yellow', None),
+            alternating_row_color=('#383734'),
+            # font="Consolas"
+            font=(None, 17),
+            enable_events=True,
+            expand_x=True,
+            expand_y=True,
+            enable_click_events=True
+            )
+        ]
+    ]
+
+    window = sg.Window('test_tablet title', layout, resizable=1)
+    while True:
+        event, value = window.read()
+                    
+        if event == "Exit" or event == sg.WIN_CLOSED:
+            break
+        if '+CLICKED+' in event:
+            # val = value['tablet'][0]
+            # pyperclip.copy(val)
+            print('12345978')
+    window.close()
+
+
+
+if __name__ == '__main__':
+    sg.theme('Dark2')
     scope = ['https://www.googleapis.com/auth/spreadsheets',
             "https://www.googleapis.com/auth/drive.file",
             "https://www.googleapis.com/auth/drive"
             ]
     creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
     client = gspread.authorize(creds)
-    sheet = client.open("geo_list").sheet1
-    return sheet
-
-
-def print_result(sheet = get_gsheet_data()):
-    # ключи смс
-    for val in sheet.col_values(1):
-        if len(val)>0:
-            print(val)
-
-
-def choose_project():
-    pass
-
-
-def show_tablet():
-    sg.theme('Dark2')
+    # sheet = client.open("geo_list").sheet1
+    sheet = client.open("Характеристики (сайты и приложения)").sheet1
+    partner_name = get_partner_names()
     
-    # there are test values // real values 'll get from "get_gsheet_data"
-    headers = ['country', 'id', 'value']
-
-    fields = [
-        ['USA, Florida', 'id 153', '{ "Success": true, "Value": { "City": "Джексонвилл", "Region": "Флорида", "Country": "США", "CountryCode": "US", "CityId": 26292, "RegionId": 1697, "CountryId": 153, "Continent": "NA", "Ip": "171.22.76.4" } }'],
-        ['Ангилья','id 10','{ "Success": true, "Value": { "Country": "Ангилья", "CountryCode": "AI", "CityId": 0, "RegionId": 0, "CountryId": 10, "Continent": "NA", "Ip": "5.62.58.9" } }'],
-        ['Антигуа и Барбуда','id 12','{ "Success": true, "Value": { "Country": "Антигуа и Барбуда", "CountryCode": "AG", "CityId": 0, "RegionId": 0, "CountryId": 12, "Continent": "NA", "Ip": "5.62.56.15" } }'],
-              ]
-
-    layout = [
-        [sg.Table(
-            values=fields, headings=headers,
-            auto_size_columns=1,
-            # display_row_numbers=1,
-            justification='right',
-            num_rows=10,
-            key='tablet',
-            row_height=35,
-            # select_mode=sg.TABLE_SELECT_MODE_BROWSE
-            )
-        ]
-    ]
-
-    window = sg.Window('test_tablet title', layout)
-    while True:
-        event, value = window.read()
-        if event == "Exit" or event == sg.WIN_CLOSED:
-            break
     
-    window.close()
+    print(partner_name)
+    show_tablet()
 
-
-
-
-
-
-show_tablet()
-
-
-
-
-
-
-
+    # ключ смс
+    # print(sheet.col_values(11))
+    
+    # основа
+    # print(sheet.col_values(13))
+    
+    print(sheet.col_values(14))
+    
 
 
 
 
 # access data
+# # print(sheet.col_values(4))
 # print(sheet.get_all_records())
 # print(sheet.row_values(2))
 # print(sheet.cell(2,1).value)
 # print(sheet.col_values(11))
-
-
-
-
-
-
 # cell_list = sheet.findall('Логин')
 # for cell in cell_list:
 #     print(cell.value, ' ', cell.row, ' ', cell.col)
--- test.py
+++ test.py
@@ -1,7 +1,92 @@
-import gspread
+# import PySimpleGUI as psg
+# psg.set_options(font=("Arial Bold", 14))
+# toprow = ['S.No.', 'Name', 'Age', 'Marks']
+# rows = [[1, 'Rajeev', 23, 78],
+#         [2, 'Rajani', 21, 66],
+#         [3, 'Rahul', 22, 60],
+#         [4, 'Robin', 20, 75]]
+# tbl1 = psg.Table(values=rows, headings=toprow,
+#    auto_size_columns=True,
+#    display_row_numbers=False,
+#    justification='center', key='-TABLE-',
+#    selected_row_colors='red on yellow',
+#    enable_events=True,
+#    expand_x=True,
+#    expand_y=True,
+#  enable_click_events=True)
+# layout = [[tbl1]]
+# window = psg.Window("Table Demo", layout, size=(715, 200), resizable=True)
+# while True:
+#    event, values = window.read()
+#    print("event:", event, "values:", values)
+#    if event == psg.WIN_CLOSED:
+#       break
+#    if '+CLICKED+' in event:
+#       psg.popup("You clicked row:{} Column: {}".format(event[2][0], event[2][1]))
+# window.close()
 
-gc = gspread.service_account(filename="C:\\repo\\test08_gsheets\\credentials.json")
 
-sh = gc.open("geo_list")
-sh = sh.create('A new spreadsheet')
-print(sh.sheet1.get('A1'))
+
+col_list = [[], [], [], []]
+test = ['123']
+
+col_list[2] = test
+
+
+print(col_list)
+# dd_list = ['Ключ в CMS', '', '', '', '', '', '', '', 'hasSnapshot = true', 'ReferalLink = ""', 'ReferalLink = ""', 'isBettingEnabled = true', 'BettingEnabledGeo = string_array']
+# print(dd_list[1:])
+    
+
+
+
+
+# import PySimpleGUI as sg
+
+# headings = ['President', 'Date of Birth']
+# data = [
+#     ['Ronald Reagan', 'February 6'],
+#     ['Abraham Lincoln', 'February 12'],
+#     ['George Washington', 'February 22'],
+#     ['Andrew Jackson', 'March 15'],
+#     ['Thomas Jefferson', 'April 13'],
+#     ['Harry Truman', 'May 8'],
+#     ['John F. Kennedy', 'May 29'],
+#     ['George H. W. Bush', 'June 12'],
+#     ['George W. Bush', 'July 6'],
+#     ['John Quincy Adams', 'July 11'],
+#     ['Garrett Walker', 'July 18'],
+#     ['Bill Clinton', 'August 19'],
+#     ['Jimmy Carter', 'October 1'],
+#     ['John Adams', 'October 30'],
+#     ['Theodore Roosevelt', 'October 27'],
+#     ['Frank Underwood', 'November 5'],
+#     ['Woodrow Wilson', 'December 28'],
+# ]
+
+# sg.theme('DarkBlue3')
+# sg.set_options(("Courier New", 12))
+
+# layout = [
+#     [sg.Button('Change')],
+#     [sg.Table(data, headings=headings, visible_column_map=[True, False],
+#         justification='left', select_mode=sg.TABLE_SELECT_MODE_BROWSE,
+#         enable_events=True, metadata=False, key='President')],
+#     [sg.Text('', size=(22, 1), key='Birthday')],
+# ]
+# window = sg.Window("Title", layout, finalize=True)
+# president = window['President']
+
+# while True:
+#     event, values = window.read()
+#     if event == sg.WINDOW_CLOSED:
+#         break
+#     elif event == 'President':
+#         index = values[event][0]
+#         window['Birthday'].update(value=f'Birthday: {data[index][1]}')
+#     elif event == 'Change':
+#         show = president.metadata = not president.metadata
+#         displaycolumns = ['President', 'Date of Birth'] if show else ['President']
+#         president.Widget.configure(displaycolumns=displaycolumns)
+
+# window.close()
